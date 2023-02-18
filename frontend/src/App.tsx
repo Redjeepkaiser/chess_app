fe1853510c9810a22352bd82b3fe0658a9420d5d
jakob <jakob.j.jeep@gmail.com>
2023-02-18 02:53:54 +0100

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Person } from "./types";
+
+function App() {
+  return (
+    <div>
+      Title
+    </div>
+  );
+}
+
+export default App;
+
